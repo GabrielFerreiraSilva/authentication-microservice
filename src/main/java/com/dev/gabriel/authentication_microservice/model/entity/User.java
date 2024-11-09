34c333cf8f634cb41d11e333793682c16b285a6e
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-09 15:29:34 -0300

    Adicionando as anotações JPA na classe Usuário

--- src/main/java/com/dev/gabriel/authentication_microservice/model/entity/User.java
+++ src/main/java/com/dev/gabriel/authentication_microservice/model/entity/User.java
@@ -1,13 +1,26 @@
 package com.dev.gabriel.authentication_microservice.model.entity;
 
 import com.dev.gabriel.authentication_microservice.model.enums.UserRole;
+import jakarta.persistence.*;
 
 import java.util.UUID;
 
+@Entity
+@Table(name = "tb_user")
 public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
+  @Column(nullable = false, unique = true)
   private String email;
+
+  @Column(nullable = false)
   private String password;
+
+  @Enumerated(EnumType.STRING)
   private UserRole role;
+
+  @Column(nullable = false)
   private Boolean isBlocked;
 }

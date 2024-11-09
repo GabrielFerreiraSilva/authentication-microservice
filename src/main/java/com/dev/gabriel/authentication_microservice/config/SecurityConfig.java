da35fbf5f4332ac880301b11d8d6c02a0f3ab1b2
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-09 19:51:12 -0300

    Adicionando código de carregamento das chaves RSA

--- src/main/java/com/dev/gabriel/authentication_microservice/config/SecurityConfig.java
+++ src/main/java/com/dev/gabriel/authentication_microservice/config/SecurityConfig.java
@@ -1,7 +1,15 @@
 package com.dev.gabriel.authentication_microservice.config;
 
+import com.dev.gabriel.authentication_microservice.util.KeyUtils;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
 
 @Configuration
 public class SecurityConfig {
@@ -10,4 +18,13 @@ public class SecurityConfig {
 
   @Value("${security.public-key-path}")
   private String publicKeyPath;
+
+  private RSAPrivateKey privateKey;
+  private RSAPublicKey publicKey;
+
+  @PostConstruct
+  public void loadKeys() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+    this.privateKey = (RSAPrivateKey) KeyUtils.decodeKey(this.privateKeyPath);
+    this.publicKey = (RSAPublicKey) KeyUtils.decodeKey(this.publicKeyPath);
+  }
 }

21dc9a7bc602397391b99023deb2a1342303afcf
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-11 11:15:47 -0300

    Implementando endpoint para o login de usuário

--- src/main/java/com/dev/gabriel/authentication_microservice/controller/AuthController.java
+++ src/main/java/com/dev/gabriel/authentication_microservice/controller/AuthController.java
@@ -1,7 +1,13 @@
 package com.dev.gabriel.authentication_microservice.controller;
 
+import com.dev.gabriel.authentication_microservice.controller.dto.LoginRequestDto;
+import com.dev.gabriel.authentication_microservice.controller.dto.LoginResponseDto;
 import com.dev.gabriel.authentication_microservice.service.LoginService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
   private final LoginService loginService;
+
+  @PostMapping("/login")
+  public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto request) {
+    LoginResponseDto response = this.loginService.loginUser(request);
+    return ResponseEntity.status(HttpStatus.OK).body(response);
+  }
 }

70fc923712bd3f706ce092dd846107c78372e5bf
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-11 11:52:34 -0300

    Adicionando anotações do JPA à entidade de refresh token

--- src/main/java/com/dev/gabriel/authentication_microservice/model/entity/RefreshToken.java
+++ src/main/java/com/dev/gabriel/authentication_microservice/model/entity/RefreshToken.java
@@ -1,11 +1,24 @@
 package com.dev.gabriel.authentication_microservice.model.entity;
 
+import jakarta.persistence.*;
+
 import java.time.Instant;
 import java.util.UUID;
 
+@Entity
+@Table(name = "tb_refresh_token")
 public class RefreshToken {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
+  @Column(unique = true, nullable = false)
   private String token;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
   private User user;
+
+  @Column(nullable = false)
   private Instant expiresAt;
 }

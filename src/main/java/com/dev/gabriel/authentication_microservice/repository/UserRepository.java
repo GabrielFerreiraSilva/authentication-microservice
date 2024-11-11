a8ebe6bb11db64486b2af60468a2e354a0c4007e
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-11 10:56:29 -0300

    Declarando query method para buscar um usuário pelo seu email

--- src/main/java/com/dev/gabriel/authentication_microservice/repository/UserRepository.java
+++ src/main/java/com/dev/gabriel/authentication_microservice/repository/UserRepository.java
@@ -3,8 +3,11 @@ package com.dev.gabriel.authentication_microservice.repository;
 import com.dev.gabriel.authentication_microservice.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
   boolean existsByEmail(String email);
+
+  Optional<User> findByEmail(String email);
 }

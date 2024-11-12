56c2de5fc1795fb43c38bdb6e45aaecee6aa4ca4
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-12 11:46:40 -0300

    Declarando query method para deletar um refresh token pelo seu usuário

--- src/main/java/com/dev/gabriel/authentication_microservice/repository/RefreshTokenRepository.java
+++ src/main/java/com/dev/gabriel/authentication_microservice/repository/RefreshTokenRepository.java
@@ -1,8 +1,11 @@
 package com.dev.gabriel.authentication_microservice.repository;
 
 import com.dev.gabriel.authentication_microservice.model.entity.RefreshToken;
+import com.dev.gabriel.authentication_microservice.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {}
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+  void deleteByUser(User user);
+}
